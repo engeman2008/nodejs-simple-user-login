9421615ceb96511cffdc12c773ec4e052e4850fc
Eman Mohammed <eman.cse2008@gmail.com>
2023-02-11 07:15:00 +0100

    add get articles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { v4: uuid } = require("uuid");
 var loadash = require("lodash");
 const { json } = require("express");
 
@@ -8,6 +8,7 @@ app.use(express.json());
 
 let users = [];
 let tokens = [];
+let articles = [];
 
 app.post("/api/user", (req, res, next) => {
   if (req.body === null) {
@@ -51,8 +52,49 @@ app.post("/api/logout", (req, res, next) => {
   tokens = loadash.dropRightWhile(tokens, ["token", token]);
   return res.status(200).send();
 });
-app.post("/api/articles", (req, res, next) => {});
-app.get("/api/articles", (req, res, next) => {});
+app.post("/api/articles", (req, res, next) => {
+  const header = req.headers.authorization;
+  const token = header.split(" ").pop();
+  const index = loadash.findIndex(tokens, (ob) => {
+    return ob.token === token;
+  });
+  if (index === -1) return res.status(401).send();
+  const user_id = loadash.nth(tokens, index).user_id;
+
+  if (req.body === null)
+    articles = loadash.concat(articles, {
+      article_id: req.body.article_id,
+      title: req.body.title,
+      content: req.body.content,
+      visibility: req.body.visibility,
+      user_id: user_id,
+    });
+  return res.status(201).send();
+});
+app.get("/api/articles", (req, res, next) => {
+  const header = req.headers.authorization;
+  let index = -1;
+  if (header) {
+    const token = header.split(" ").pop();
+    index = loadash.findIndex(tokens, (ob) => {
+      return ob.token === token;
+    });
+  }
+
+  let query_articles = [];
+  if (index === -1) {
+    query_articles = loadash.filter(articles, (article) => {
+      return article.visibility === "public";
+    });
+  } else {
+    query_articles = loadash.filter(articles, (article) => {
+      return (
+        article.visibility === "public" || article.visibility === "logged_in"
+      );
+    });
+  }
+  return res.status(200).json(query_articles);
+});
 
 app.listen(3000, () => {
   console.log("started");
